577f99e1cac065263aa86b8dcec24e1f1913ccb0
David Bauer <david.bauer@open-xchange.com>
2023-04-05 11:28:27 +0200

    Add: CVE id to link converter

--- src/handlebars-setup.js
+++ src/handlebars-setup.js
@@ -1,4 +1,8 @@
-const addLinks = (str) => str.replace(/([A-Z]{2,9}-\d+)/g, '[`$1`](https://jira.open-xchange.com/browse/$1)')
+
+
+const addLinks = str => str
+  .replace(/(CVE-\d+-\d+)/g, '[`$1`](https://www.cve.org/CVERecord?id=$1)')
+  .replace(/^(?![A-Z]{2,9}-\d+-$)([A-Z]{2,9}-\d+)/g, '[`$1`](https://jira.open-xchange.com/browse/$1)')
 const stripKeywords = (str) => addLinks(str.replace(/^(add(ed)?|change(d)?|deprecate(ed)?|fix(ed)?|remove(d)?|security)?:\s+?/i, ''))
 const excise = (str) => {
   const entry = str.split('Changelog:\n')[1]
@@ -9,4 +13,4 @@ const compile = ({ subject, message, shorthash, href }) => {
   return [`${stripKeywords(subject)} [\`${shorthash}\`](${href})`, ...excise(message)].join('\n  ')
 }
 
-module.exports = (Handlebars) => Handlebars.registerHelper('custom', (context) => compile(context))
+module.exports = Handlebars => Handlebars.registerHelper('custom', (context) => compile(context))
