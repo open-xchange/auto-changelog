185497a366b95b657c55bd4d362e2e483195a52a
David Bauer <david.bauer@open-xchange.com>
2023-04-05 23:20:32 +0200

    Fix: Jira id to link converter

--- src/handlebars-setup.js
+++ src/handlebars-setup.js
@@ -2,7 +2,7 @@
 
 const addLinks = str => str
   .replace(/(CVE-\d+-\d+)/g, '[`$1`](https://www.cve.org/CVERecord?id=$1)')
-  .replace(/^(?![A-Z]{2,9}-\d+-$)([A-Z]{2,9}-\d+)/g, '[`$1`](https://jira.open-xchange.com/browse/$1)')
+  .replace(/(?![A-Z]{2,9}-\d+-$)([A-Z]{2,9}-\d+)/g, '[`$1`](https://jira.open-xchange.com/browse/$1)')
 const stripKeywords = (str) => addLinks(str.replace(/^(add(ed)?|change(d)?|deprecate(ed)?|fix(ed)?|remove(d)?|security)?:\s+?/i, ''))
 const excise = (str) => {
   const entry = str.split('Changelog:\n')[1]
